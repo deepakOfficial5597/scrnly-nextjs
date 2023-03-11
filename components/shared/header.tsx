b7a1772ecf19dedb6e6544399198e16bef48641b
Deepak5597 <deepakbisht5597@gmail.com>
2023-03-11 23:26:25 +0530

    Removed Heaer logic to show login logout button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
-import { useSession, signIn, signOut } from "next-auth/react"
 
 const Header = () => {
-    const { data: session, status } = useSession()
     return (
         <header className="flex justify-between items-center min-h-[50px] px-10">
             <div className="flex flex-row items-center gap-2">
@@ -11,13 +9,7 @@ const Header = () => {
                 <h1 className="font-bold">Scrnly</h1>
             </div>
             <div className="flex gap-2">
-                {
-                    (status === "authenticated") && <p className="text-sm">Welcome {session?.user?.name}</p>
-                }
-                {
-                    session ? <Link href="logout" className="text-sm mr-2" onClick={() => signOut()}> Logout </Link> : <Link href="login" className="text-sm mr-2" onClick={() => signIn()}> Login </Link>
-                }
-                
+                <Link href="/how-to" className="text-sm mr-2"> How To ? </Link>
             </div>
         </header>
     )
