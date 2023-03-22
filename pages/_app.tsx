4ae26b4b004625a7b69011fcc899e52397b69254
Deepak5597 <deepakbisht5597@gmail.com>
2023-03-23 00:04:12 +0530

    Google Analytics Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GA_TRACKING_ID } from "../libs/gtag";
+
+const isProduction = true;
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      { isProduction && (<>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </>)}
+      <Component {...pageProps} />
+    </>
   )
 }
